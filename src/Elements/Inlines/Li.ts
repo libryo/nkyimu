2656b27c6543071c4e0038f3dc23c4e90d6adf7a
David Mjomba <smodavprivate@gmail.com>
2018-12-05 10:34:17 +0300

    adds list item (#28)

---
+++ src/Elements/Inlines/Li.ts
@@ -0,0 +1,41 @@
+import { AbstractNode } from "../../Abstracts/AbstractNode";
+import { AttributeGroupItem } from "../../Interfaces/AttributeGroupItem";
+import { NodeRules } from "../../Interfaces/NodeRules";
+import { InlineCM } from "../../ElementGroups/InlineCM";
+import { Optvalue, Coreopt } from "../../AttributeGroups";
+
+/**
+ * The element li is an HTML element and is used in Akoma Ntoso as in HTML,
+ * for the generic list item (not a pattern)
+ */
+export class Li extends AbstractNode {
+  abbreviation = 'li';
+
+  getNodeName(): string { return 'li'; }
+
+  readonly CHILDREN_MAP: NodeRules = {
+    liChoice: {
+      minOccur: 0,
+      choice: true,
+      options: {
+        liChoiceGroup: {
+          minOccur: 0,
+          options: {
+            ...(new InlineCM()).CHILDREN_MAP,
+          }
+        },
+        'ul': { minOccur: 0, options: {} },
+        'ol': { minOccur: 0, options: {} },
+        'p': { minOccur: 0, options: {} },
+      }
+    },
+    '': { minOccur: 0, options: {} },
+  };
+
+  readonly SEQUENCE: string[] = [];
+
+  readonly ATTRIBUTE_GROUPS: AttributeGroupItem[] = [
+    ...(new Optvalue()).items,
+    ...(new Coreopt()).items,
+  ];
+}
